9a13ef788da354ca55265e5f3028f5ae9c8c7eaf
Nikola <nikse235@student>
2014-12-15 13:47:30 +0100

    codetest2 kom inte med sist

---
+++ src/testpgm/codetest2.d
@@ -0,0 +1,37 @@
+program main;
+
+procedure write_int(val : integer);
+const
+    ASCII0 = 48;                { ascii value of '0' }
+    MINUS = 45;
+var
+    c : integer;
+    r : real;
+    buf : array[10] of integer; { enough for 10 digits }
+    bufp : integer;
+begin
+    r := 10;
+    if (val = 0) then
+        write(ASCII0);
+        return;
+    end;
+    if (val < 0) then
+        write(MINUS);
+        val := -val;
+    end;
+    bufp := 0;
+    while val > 0 do
+        c := val mod 10;
+        buf[bufp] := c + ASCII0;
+        bufp := bufp + 1;
+        val := val div 10;
+    end;
+    while (bufp > 0) do
+        bufp := bufp - 1;
+        write(buf[bufp]);
+    end;
+end;
+
+begin
+    write_int(23);
+end.
